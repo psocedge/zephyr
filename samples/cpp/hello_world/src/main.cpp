fb544d833b6c1f403c1b87c06ba885e73f441501
Zhilin Ouyang <Psocedge@gmail.com>
2024-06-23 03:48:55 +0000

    adding module math lib

--- samples/cpp/hello_world/src/main.cpp
+++ samples/cpp/hello_world/src/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <iostream>
+#include <platform_math.hpp>
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,7 +15,8 @@ extern "C" {
 
 int main(void)
 {
-	std::cout << "result: " << foo_sample(2) << std::endl;
+	std::cout << "SigmoidF32: " << arm::math::MathUtils::SigmoidF32(2.3) << std::endl;
+	std::cout << "result: " << foo_sample(2) << std::endl;	
 	std::cout << "Hello, C++ world! " << CONFIG_BOARD << std::endl;
 	return 0;
 }
